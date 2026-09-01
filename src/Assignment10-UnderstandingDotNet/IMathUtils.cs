5a878af60f565aee746f88aaad6f577216c01882
Shubha Shree Sathish <shubhashree.sathish@solitontech.com>
2026-09-01 19:32:19 +0530

    feat: Add Learning.md, Calculator class

---
+++ src/Assignment10-UnderstandingDotNet/IMathUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    public interface IMathUtils
+    {
+        public int Add(int number1, int number2);
+        public int Subtract(int number1, int number2);
+        public int Multiply(int number1, int number2);
+        public int Divide(int number1, int number2);
+    }
+}
---
+++ src/Assignment10-UnderstandingDotNet/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Debugging
+{
+    internal class Program
+    {
+        static int num;
+        static void Main(string[] args)
+        {
+            Program.Run();
+        }
+
+        static void Run()
+        {
+            MathUtils utilHandler = new MathUtils();
+
+            Console.WriteLine("Enter operand 1: ");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter operand 2: ");
+            int number2 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine($"Addition of {number1} and {number2} results {utilHandler.Add(number1,number2)}");
+
+            Console.WriteLine($"Subtraction of {number1} and {number2} results {utilHandler.Subtract(number1, number2)}");
+
+            Console.WriteLine($"Multiplication of {number1} and {number2} results {utilHandler.Multiply(number1, number2)}");
+
+            Console.WriteLine($"Division of {number1} and {number2} results {utilHandler.Divide(number1, number2)}");
+        }
+    }
+}
