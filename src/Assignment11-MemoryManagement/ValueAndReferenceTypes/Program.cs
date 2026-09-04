c8c5e4a3013d7fdb2a7c69a0866395473d975d10
Shubha Shree Sathish <shubhashree.sathish@solitontech.com>
2026-09-04 10:34:35 +0530

    feat: Add Value and Reference Types demonstration

---
+++ src/Assignment11-MemoryManagement/ValueAndReferenceTypes/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace Assignments
+{
+    internal class Program
+    {
+        public struct PersonStruct
+        {
+            public PersonStruct(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class PersonClass
+        {
+            public PersonClass(string name, int age)
+            {
+                this.Name = name;
+                this.Age = age;
+            }
+
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        static void DisplayDash()
+        {
+            Console.WriteLine("--------------------------------------------");
+        }
+
+        static void Display(PersonStruct person1, PersonClass person2)
+        {
+            Program.DisplayDash();
+            Console.WriteLine("Contents instide PersonStruct:");
+            Console.WriteLine($"Name: {person1.Name}\nAge:{person1.Age}");
+
+            Program.DisplayDash();
+            Console.WriteLine("Contents instide PersonClass:");
+            Console.WriteLine($"Name: {person2.Name}\nAge:{person2.Age}");
+        }
+
+        static void Modify(PersonStruct person1, PersonClass person2)
+        {
+            person1.Name = "Shree";
+            person1.Age = 15;
+
+            person2.Name = "Shree";
+            person2.Age = 15;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter age: ");
+            int age = int.Parse(Console.ReadLine());
+
+
+            PersonStruct person1 = new PersonStruct(name, age);
+            PersonClass person2 = new PersonClass(name, age);
+
+            Console.WriteLine("\nContents before modifying:");
+            Program.Display(person1, person2);
+
+            Program.Modify(person1, person2);
+
+            Program.DisplayDash();
+            Console.WriteLine("\nContents after modifying:");
+            Program.Display(person1, person2);
+        }
+    }
+}
