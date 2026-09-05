42bf1ccb84217366ccf27f079020e75b7d32c00d
Shubha Shree Sathish <shubhashree.sathish@solitontech.com>
2026-09-05 12:48:52 +0530

    docs: Add xml for struct, class

--- src/Assignment11-MemoryManagement/ValueAndReferenceTypes/PersonClass.cs
+++ src/Assignment11-MemoryManagement/ValueAndReferenceTypes/PersonClass.cs
@@ -1,12 +1,27 @@
+/// <summary>
+/// A class representing person.
+/// </summary>
 public class PersonClass
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PersonClass"/> struct.
+    /// </summary>
+    /// <param name="name">Name of the person.</param>
+    /// <param name="age">Age of the person.</param>
     public PersonClass(string name, int age)
     {
         this.Name = name;
         this.Age = age;
     }
 
+    /// <summary>
+    /// Name of the person.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// Age of the person.
+    /// </summary>
+    /// <value>Person's age.</value>
     public int Age { get; set; }
 }
--- src/Assignment11-MemoryManagement/ValueAndReferenceTypes/PersonStruct.cs
+++ src/Assignment11-MemoryManagement/ValueAndReferenceTypes/PersonStruct.cs
@@ -1,12 +1,27 @@
+/// <summary>
+/// A struct representing person.
+/// </summary>
 public struct PersonStruct
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PersonStruct"/> struct.
+    /// </summary>
+    /// <param name="name">Name of the person.</param>
+    /// <param name="age">Age of the person.</param>
     public PersonStruct(string name, int age)
     {
         this.Name = name;
         this.Age = age;
     }
 
+    /// <summary>
+    /// Name of the person.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// Age of the person.
+    /// </summary>
+    /// <value>Person's age.</value>
     public int Age { get; set; }
 }
